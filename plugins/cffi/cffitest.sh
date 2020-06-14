5166f9e220f2a4c14f40d4a0a359d3d8e31b9abb
Daniel Holth <dholth@fastmail.fm>
2020-06-14 10:29:02 -0400

    add do-all cffitest.sh

--- plugins/cffi/cffitest.sh
+++ plugins/cffi/cffitest.sh
@@ -2,15 +2,17 @@
 # Run from base uwsgi directory
 set -e
 # export C_INCLUDE_PATH=/usr/local/opt/openssl\@1.1/include
-python ./uwsgiconfig.py -p plugins/cffi nolang
+# build uwsgi
+python uwsgiconfig.py --build nolang
+# build plugin
+python uwsgiconfig.py -p plugins/cffi nolang
+# run
 ./uwsgi --plugin cffi -T \
     --http-socket 0.0.0.0:8080 \
     --single-interpreter \
     --env=PYTHONPATH=$HOME/prog/uwsgi:$HOME/prog/uwsgi/plugins/cffi \
-    --cffi-wsgi=$PWD/helloworld.py \
+    --cffi-wsgi=$PWD/examples/welcome.py \
     --manage-script-name \
-    --mount=/app=helloworld:application \
-    --mount=/app2=$PWD/helloworld.py \
+    --mount=/app=$PWD/examples/welcome3.py \
     --chdir=$VIRTUAL_ENV/bin \
-    --master \
-    --touch-reload=$HOME/prog/uwsgi/helloworld.py
+    --master
