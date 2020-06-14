5c87dc55714b46d88283ea6f3f8b3b7e372a8e6d
Daniel Holth <dholth@fastmail.fm>
2020-06-14 10:20:30 -0400

    update scripts

--- plugins/cffi/chattest.sh
+++ plugins/cffi/chattest.sh
@@ -4,16 +4,17 @@ set -e
 export PYTHONPATH=$PWD:$PWD/plugins/cffi
 export C_INCLUDE_PATH=/usr/local/opt/openssl\@1.1/include
 python ./uwsgiconfig.py -p plugins/cffi nolang
-./uwsgi \
+authbind ./uwsgi \
   --master \
   --enable-threads \
   --plugin=cffi \
   --cffi-init=cffi_setup_asyncio \
   --async=32 \
-  --http-socket=[::]:8080 \
+  --http-socket=[::]:80 \
   --manage-script-name \
   --mount=/=starlettetest:app \
   --mount=/wsgi=$PWD/examples/welcome3.py \
+  --mount=/welcome=$PWD/examples/welcome.py \
   --chdir=$VIRTUAL_ENV/bin \
   --touch-reload $PWD/starlettetest.py \
   --touch-reload $PWD/plugins/cffi/cffi_asyncio.py
